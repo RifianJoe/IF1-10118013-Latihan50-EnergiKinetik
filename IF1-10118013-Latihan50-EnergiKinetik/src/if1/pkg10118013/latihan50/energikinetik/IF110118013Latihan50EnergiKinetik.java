71a34c10fb8db3e21e6d4a2a24842e8332f8ee5d
Rifian Joe Premilenio <55788355+RifianJoe@users.noreply.github.com>
2019-11-07 21:37:21 +0800

    Add files via upload

---
+++ IF1-10118013-Latihan50-EnergiKinetik/src/if1/pkg10118013/latihan50/energikinetik/IF110118013Latihan50EnergiKinetik.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118013.latihan50.energikinetik;
+
+/**
+ *
+ * @author Rifian
+ * Nama : Rifian Joe Premilenio
+ * Kelas : IF-01
+ * NIM : 10118013
+ */
+public class IF110118013Latihan50EnergiKinetik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        EnergiKinetik ek = new EnergiKinetik();
+        ek.tampilHasil();
+    }
+    
+}
